2924226a0f8babc42ddbe9aa669f50f60d4f2429
FinAccTech <finacctechnologies@gmail.com>
2025-06-30 19:03:37 +0530

    30-06

--- Saas Queries/Saas Creation Query.sql
+++ Saas Queries/Saas Creation Query.sql
@@ -7,20 +7,20 @@
 INSERT INTO Registered_Clients (Client_Code, Client_Name, Address, State, Mobile, Alternate_Mobile, Email, Contact_Person, Creation_Date, Subscription_Start, Subscription_End, App_Login, App_Pwd, Db_Name, Status, Version_Type, Own_Server)
 VALUES 
 (
-	'FS2025062465', --Client Code
-	'Jayam gold bankers',  -- ClientName
-	'No 5 , sethurathinapuram , manapparai', -- Address
+	'FS2025062866', --Client Code
+	'Venkatachalapathi Finance',  -- ClientName
+	'SIHS Colony Road, Singanallur, Coimbatore', -- Address
 	'Tamilnadu', -- State
-	'9360258177', -- Mobile
-	'9597465222', -- Alternate Mobile
-	'jayasurya22.sgp@gmail.com', -- Email
-	'Jaya surya', -- Contact Person
-	20250624, -- Creation Date
-	20250624, -- Subscription Start
-	20260623, -- Subscription End
+	'9742763030', -- Mobile
+	'9443357392', -- Alternate Mobile
+	'venkatgoldfinance@gmail.com', -- Email
+	'Kabilan Palanivel', -- Contact Person
+	20250628, -- Creation Date
+	20250628, -- Subscription Start
+	20260627, -- Subscription End
 	'', -- App Login
 	'', -- App_Pwd
-	'FS2025062465', -- DbName
+	'FS2025062866', -- DbName
 	1, -- Status
 	3, -- Version Type
 	0 -- Own_Serer
